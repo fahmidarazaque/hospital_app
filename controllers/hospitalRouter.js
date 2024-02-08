5ae82089a0181567fbf5504b6dd28a93f5621920
fahmidarazaque <fahmidank99@gmail.com>
2024-02-09 01:20:44 +0530

    search

--- controllers/hospitalRouter.js
+++ controllers/hospitalRouter.js
@@ -18,4 +18,9 @@ router.get("/hospital_view",async(req,res)=>{
     res.send("hello")
 
 })
+router.post("/search",async(req,res)=>{
+    let input=req.body
+    let data=await hospitalModel.find(input)
+    res.json(data)
+})
 module.exports=router
